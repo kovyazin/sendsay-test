3e29da4f254c5a1e8a65ae928d873fc2b9366b30
Vasya Peregarkin <slava.kovazin@gmail.com>
2019-12-24 19:06:10 +0500

    Удалил файл стилей для компонента Logo

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,16 +1,13 @@
 /* Import libraries */
 import React from 'react';
 
-/* Import styles */
-import './Logo.css';
-
 /* Import images */
 import logo from './logo.svg';
 
 const Logo = props => {
   return (
-    <div className="logo">
-      <img src={logo} alt="" />
+    <div>
+      <img src={logo} alt="Logo" />
     </div>
   );
 };
