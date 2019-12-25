4c4f00bbaefe12719e95af7252b6fc109396ff3b
Vasya Peregarkin <slava.kovazin@gmail.com>
2019-12-25 21:57:14 +0500

    Updates

---
+++ precache-manifest.12a5704639707585f5a41feaf6eb2d3b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3282bf0a6ecbc394fc6a5fe5e26c44",
+    "url": "/sendsay-test/index.html"
+  },
+  {
+    "revision": "27d729b5b315bd7daff9",
+    "url": "/sendsay-test/static/css/main.3553538b.chunk.css"
+  },
+  {
+    "revision": "f71b0eaa2f1d8a8b1a11",
+    "url": "/sendsay-test/static/js/2.4fe147b0.chunk.js"
+  },
+  {
+    "revision": "931f634827e16235d99e4ff139d33883",
+    "url": "/sendsay-test/static/js/2.4fe147b0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "27d729b5b315bd7daff9",
+    "url": "/sendsay-test/static/js/main.2554c0cd.chunk.js"
+  },
+  {
+    "revision": "0a97427ad6296d0b9b26",
+    "url": "/sendsay-test/static/js/runtime-main.8e0432d1.js"
+  },
+  {
+    "revision": "441c0a575c3ef693404a89662b05ae95",
+    "url": "/sendsay-test/static/media/clip.441c0a57.svg"
+  },
+  {
+    "revision": "ac8c01c5279bc70a09cd77ad02271ea9",
+    "url": "/sendsay-test/static/media/clip.ac8c01c5.svg"
+  },
+  {
+    "revision": "542991012b0e7493099d5e4d27331512",
+    "url": "/sendsay-test/static/media/logo.54299101.svg"
+  },
+  {
+    "revision": "04c58a03382471b2876698da5935c43e",
+    "url": "/sendsay-test/static/media/trash.04c58a03.svg"
+  }
+]);
